4a97b30543e3149c1995ef5e2836bb0243e08999
airu3 <sugar.tom5310@gmail.com>
2024-01-28 03:42:14 +0900

    Refactor ShiritoriModel class and update StringUtil usage

--- src/main/java/model/ShiritoriModel.java
+++ src/main/java/model/ShiritoriModel.java
@@ -1,39 +1,43 @@
 package model;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import util.StringUtil;
 
 public class ShiritoriModel {
-    private List<String> wordHistory = new ArrayList<>();
-    private String cpuWord = "";
-    private boolean isWork = false;
+	private List<String> wordHistory = new ArrayList<>();
+	private String cpuWord = "";
+	private boolean isWork = false;
 
-    public String playShiritori(String userMsg) {
-        List<String> words = new ArrayList<>();
-        List<String> links = new ArrayList<>();
+	public String playShiritori(String userMsg) {
+		List<String> words = new ArrayList<>();
+		List<String> links = new ArrayList<>();
 
-        String[] changes = strChange(userMsg, -1);
+		String[] changes = StringUtil.processJapaneseWord(userMsg, -1);
 
-        fetchWordsFromWikipedia(changes[0], words, links);
-        fetchWordsFromWikipedia(changes[1], words, links);
+		fetchWordsFromWikipedia(changes[0], words, links);
+		fetchWordsFromWikipedia(changes[1], words, links);
 
-        if (words.isEmpty()) {
-            return "負けました";
-        }
+		if (words.isEmpty()) {
+			return "負けました";
+		}
 
-        int random = (int) (Math.random() * words.size());
-        cpuWord = words.get(random);
-        String wikiLink = links.get(random);
+		int random = (int) (Math.random() * words.size());
+		cpuWord = words.get(random);
+		String wikiLink = links.get(random);
 
-        if (strChange(cpuWord, -1)[0].equals("ん")) {
-            do {
-                words.remove(cpuWord);
-                random = (int) (Math.random() * words.size());
-                cpuWord = words.get(random);
-                wikiLink = links.get(random);
-            } while (strChange(cpuWord, -1)[0].equals("ん"));
-        }
+		if (strChange(cpuWord, -1)[0].equals("ん")) {
+			do {
+				words.remove(cpuWord);
+				random = (int) (Math.random() * words.size());
+				cpuWord = words.get(random);
+				wikiLink = links.get(random);
+			} while (strChange(cpuWord, -1)[0].equals("ん"));
+		}
 
-        return cpuWord + "," + wikiLink;
-    }
+		return cpuWord + "," + wikiLink;
+	}
 
 }
