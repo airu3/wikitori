f9fc8b623604bdd16a728d8773fdadcce5d67424
airu3 <sugar.tom5310@gmail.com>
2024-01-31 03:59:46 +0900

    Add NG word filtering and improve word selection logic

---
+++ src/main/java/util/NgWordManager.java
@@ -0,0 +1,51 @@
+package util;
+
+import java.util.ArrayList;
+
+public class NgWordManager {
+	private static ArrayList<String> ngWords;
+	static {
+		addNgWord("アナル");
+		addNgWord("アダルトビデオ");
+		addNgWord("アダルトゲーム");
+		addNgWord("うつ病");
+		addNgWord("オナニー");
+		addNgWord("オナホール");
+		addNgWord("コンドーム");
+		addNgWord("ばにしゅ! 〜おっぱいの消えた王国〜");
+		addNgWord("ポルノ映画");
+		addNgWord("まんこ");
+		addNgWord("リベンジポルノ");
+	}
+
+	// NGワードを追加
+	public static void addNgWord(String word) {
+		ngWords.add(word);
+	}
+
+	// NGワードを削除
+	public static void removeNgWord(String word) {
+		ngWords.remove(word);
+	}
+
+	// 指定したワードがNGワードかどうかをチェック
+	public static boolean isNgWord(String word) {
+		return ngWords.contains(word);
+	}
+
+	// 含まれていますか？
+	public static boolean isContain(String word) {
+		return ngWords.contains(word);
+	}
+
+	// NGワードの一覧を取得
+	public static ArrayList<String> getNgWords() {
+		return ngWords;
+	}
+
+	// NGワードの一覧を設定
+	public static void setNgWords(ArrayList<String> ngWords) {
+		NgWordManager.ngWords = ngWords;
+	}
+
+}
