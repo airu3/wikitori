3166f1fee9940f956100e6cb42b54d13f928a3dc
airu3 <sugar.tom5310@gmail.com>
2024-02-02 20:40:01 +0900

    Add and update NG words in NgWordManager class

--- src/main/java/util/NgWordManager.java
+++ src/main/java/util/NgWordManager.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 public class NgWordManager {
 	private static List<String> ngWords;
+	// wikipedia検索時に1/300の確率で見事にヒットしてしまったので追加
 	static {
 		ngWords = new ArrayList<>();
 		addNgWord("アナル");
 		addNgWord("アダルトビデオ");
 		addNgWord("アダルトゲーム");
 		addNgWord("うつ病");
+		addNgWord("うつ病の治療");
 		addNgWord("オナニー");
 		addNgWord("オナホール");
 		addNgWord("コンドーム");
@@ -20,32 +22,61 @@ public class NgWordManager {
 		addNgWord("リベンジポルノ");
 	}
 
-	// NGワードを追加
+	/**
+	 * NGワードを追加
+	 * 
+	 * @param word 追加するNGワード
+	 */
 	public static void addNgWord(String word) {
 		ngWords.add(word);
 	}
 
-	// NGワードを削除
+	/**
+	 * NGワードを削除
+	 * 
+	 * @param word 削除するNGワード
+	 * 
+	 */
 	public static void removeNgWord(String word) {
 		ngWords.remove(word);
 	}
 
-	// 指定したワードがNGワードかどうかをチェック
+	/**
+	 * NGワードかどうかをチェック
+	 * 
+	 * @param word チェックするワード
+	 * @return NGワードの場合はtrue
+	 * 
+	 */
 	public static boolean isNgWord(String word) {
 		return ngWords.contains(word);
 	}
 
-	// 含まれていますか？
-	public static boolean isContain(String word) {
+	/**
+	 * NGワードを含むかどうかをチェック
+	 * 
+	 * @param word チェックするワード
+	 * @return NGワードを含む場合はtrue
+	 * 
+	 */
+	public static boolean isContains(String word) {
 		return ngWords.contains(word);
 	}
 
-	// NGワードの一覧を取得
+	/**
+	 * NGワードの一覧を取得
+	 * 
+	 * @return NGワードの一覧
+	 */
 	public static List<String> getNgWords() {
 		return ngWords;
 	}
 
-	// NGワードの一覧を設定
+	/**
+	 * NGワードの一覧を設定
+	 * 
+	 * @param ngWords NGワードの一覧
+	 */
 	public static void setNgWords(List<String> ngWords) {
 		NgWordManager.ngWords = ngWords;
 	}
