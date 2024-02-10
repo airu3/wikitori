50b163bd5af519194c1d3436d7f2a2eca37da7ee
airu3醤 <sugar.tom5310@gmail.com>
2024-02-10 23:59:59 +0900

    ShiritoriManagerクラスの追加

---
+++ src/main/java/model/ShiritoriManager.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import util.StringUtil;
+
+public class ShiritoriManager {
+	private static List<ChatHistory> chatHistoryList = new ArrayList<ChatHistory>();
+	private static StringUtil stringUtil = new StringUtil();
+
+}
