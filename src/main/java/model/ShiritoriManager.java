3679d796faf4d5d285e88b12bb02c404f07166b1
airu3 <sugar.tom5310@gmail.com>
2024-06-30 19:44:44 +0900

    chore: Remove unused import in ShiritoriManager class

--- src/main/java/model/ShiritoriManager.java
+++ src/main/java/model/ShiritoriManager.java
@@ -8,5 +8,4 @@ import util.StringUtil;
 public class ShiritoriManager {
 	private static List<ChatHistory> chatHistoryList = new ArrayList<ChatHistory>();
 	private static StringUtil stringUtil = new StringUtil();
-
 }
