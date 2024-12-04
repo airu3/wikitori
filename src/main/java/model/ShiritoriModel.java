664c2b6dcabf5aab20d1d75bb7a8f28c5c311a1f
airu3 <sugar.tom5310@gmail.com>
2024-12-04 23:42:44 +0900

    修正: Wikipediaから取得する単語情報の数を20に減少し、チャットボットのメッセージを改善。新しい単語取得機能を追加。

--- src/main/java/model/ShiritoriModel.java
+++ src/main/java/model/ShiritoriModel.java
@@ -42,8 +42,8 @@ public class ShiritoriModel {
 			filterWords.addAll(usedWords);
 
 			// それぞれの単語の情報を取得する
-			futures.add(WikipediaFetcher.fetchWordInfo(changes[0], 100, filterWords));
-			futures.add(WikipediaFetcher.fetchWordInfo(changes[1], 100, filterWords));
+			futures.add(WikipediaFetcher.fetchWordInfo(changes[0], 20, filterWords));
+			futures.add(WikipediaFetcher.fetchWordInfo(changes[1], 20, filterWords));
 
 			// すべてのCompletableFutureが完了するまで待機する
 			CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
